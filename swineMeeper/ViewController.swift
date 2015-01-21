d0476e015642e24b27b96310af62cdaf440702d8
Naomi Himley <naomihimley@gmail.com>
2015-01-20 23:47:21 -0600

    Initial Commit

---
+++ swineMeeper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swineMeeper
+//
+//  Created by Naomi Himley on 1/20/15.
+//  Copyright (c) 2015 Naomi Himley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
